bc30121a599d9f8e2add746fddac4a16a7cbe622
Kyoko <108702599+KyoMatias@users.noreply.github.com>
2024-01-31 01:39:18 +0800

    first commit

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private GameObject m_mainCamera;
+     [SerializeField] private GameObject m_menuCamera;
+     [SerializeField] private GameObject m_menuUI;
+
+    [SerializeField]private Animator camerapan;
+    [SerializeField] private Animator menupan;
+
+    public delegate void PlayAnim();
+    public static PlayAnim PlayAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        m_menuCamera.SetActive(true);
+        m_mainCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+
+    public void TestAnimation()
+    {
+        camerapan.Play("camerapan");
+        menupan.Play("StowAway");
+        StartCoroutine(ActivatePlayerCam());
+    }
+
+    IEnumerator ActivatePlayerCam()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        yield return new WaitForSeconds(1.7f);
+        m_mainCamera.SetActive(true);
+        m_menuCamera.SetActive(false);
+        m_menuUI.SetActive(false);
+        yield return null;
+    }
+
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
